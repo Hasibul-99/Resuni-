927d04a8435952c43bbb672e8943b6e58be6cc7f
Hasibul-99 <hasibul_uap@outlook.com>
2019-08-30 22:32:00 +0600

    navigation add in the page

---
+++ src/components/Navbar.vue
@@ -0,0 +1,59 @@
+<template>
+    <nav>
+        <v-app-bar-nav-icon @click="showSidebar" v-if="!drawer"></v-app-bar-nav-icon>
+        <v-icon @click="showSidebar" v-else>mdi-close</v-icon>
+
+        <v-navigation-drawer v-model="drawer" app class="padless">
+            <v-card
+                max-height="240"
+                class="mx-auto">
+                <v-img
+                    class="white--text"
+                    height="240px"
+                    src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+                    > </v-img>
+                <v-list disabled>
+                    <v-subheader>REPORTS</v-subheader>
+                    <v-list-item-group v-model="item" color="primary">
+                        <v-list-item v-for="(item, i) in items" :key="i" >
+                            <v-list-item-icon>
+                                <v-icon v-text="item.icon"></v-icon>
+                            </v-list-item-icon>
+                            <v-list-item-content>
+                                <v-list-item-title v-text="item.text"></v-list-item-title>
+                            </v-list-item-content>
+                        </v-list-item>
+                    </v-list-item-group>
+                </v-list>
+                
+            </v-card>
+        </v-navigation-drawer>
+    </nav>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            drawer: false,
+            items: [
+                {icon: "mdi-account", text: 'Home'},
+                {icon: "mdi-arrow-left-right", text: 'ABOUT'},
+                {icon: "mdi-layers", text: 'EDUCATION'},
+                {icon: "mdi-chart-bar", text: 'SKILLS'},
+                {icon: "mdi-castle ", text: 'EXPERIENCE'},
+                {icon: "mdi-google-podcast", text: 'PORTFOLLIOS'},
+                {icon: "mdi-bottle-tonic-outline", text: 'INTEREST'},
+                {icon: "mdi-arrow-expand-right", text: 'TESTIMONIALS'},
+                {icon: "mdi-arrow-left-right-bold-outline", text: 'PRICING'},
+                {icon: "mdi-arrow-left-box", text: 'BLOG'},
+                {icon: "mdi-arrow-up-bold-circle-outline", text: 'CONTACT'},
+            ]
+        }
+    },
+    methods: {
+        showSidebar() {
+            this.drawer = !this.drawer;
+        }
+    }
+}
+</script>
