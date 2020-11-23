ae54586b58d396dcf0f8209a4ab06a00e5173926
Hasibul Hasan <hasibul_uap@outlook.com>
2020-11-23 23:45:50 +0600

    deign update in skills

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -10,93 +10,44 @@
                 <v-card class="skills-card">
                     <v-container class="pa-10">
                         <v-row class="mb-6 skills-row">
-                            <v-col class="skills-type" sm="12" md="4" xs="12">
-                                <h4>Professional</h4>
-                                <div class="text-left mt-10">
-                                    HTML
-                                    <v-progress-linear buffer-value="60" value="40"
-                                        stream color="white" class="mt-3" >
-                                    </v-progress-linear>
-                                    <br>
-                                    CSS
-                                    <v-progress-linear buffer-value="60" value="40"
-                                        stream color="white" class="mt-3" >
-                                    </v-progress-linear>
-                                    <br>
-                                    JavaScript
-                                    <v-progress-linear buffer-value="60" value="40"
-                                        stream color="white" class="mt-3" >
-                                    </v-progress-linear>
-                                    <br>
-                                    PHP
-                                    <v-progress-linear buffer-value="60" value="40"
-                                        stream color="white" class="mt-3" >
-                                    </v-progress-linear>
-                                    <br>
-                                    Node.js
-                                    <v-progress-linear buffer-value="60" value="40"
-                                        stream color="white" class="mt-3" >
-                                    </v-progress-linear>
+                            <v-col v-for="i in 15" :key="i" class="skills-type" sm="12" md="3" xs="12">
+                                <div class="container skills-container">
+                                    <div class="avatar">
+                                        <a href="#!">
+                                            <img src="/img/Javascript-shield.svg"  alt="Skytsunami" />
+                                        </a>
+                                    </div>
                                 </div>
                             </v-col>
 
-                            <v-col class="skills-type" sm="12" md="4" xs="12">
-                                <h4>Professional</h4>
-                                <div class="text-left mt-10">
-                                    HTML
-                                    <v-progress-linear buffer-value="60" value="40"
-                                        stream color="white" class="mt-3" >
-                                    </v-progress-linear>
-                                    <br>
-                                    CSS
-                                    <v-progress-linear buffer-value="60" value="40"
-                                        stream color="white" class="mt-3" >
-                                    </v-progress-linear>
-                                    <br>
-                                    JavaScript
-                                    <v-progress-linear buffer-value="60" value="40"
-                                        stream color="white" class="mt-3" >
-                                    </v-progress-linear>
-                                    <br>
-                                    PHP
-                                    <v-progress-linear buffer-value="60" value="40"
-                                        stream color="white" class="mt-3" >
-                                    </v-progress-linear>
-                                    <br>
-                                    Node.js
-                                    <v-progress-linear buffer-value="60" value="40"
-                                        stream color="white" class="mt-3" >
-                                    </v-progress-linear>
+                            <v-col class="skills-type" sm="12" md="3" xs="12">
+                                
+                                <div class="container skills-container">
+                                    <div class="avatar">
+                                        <a href="#!">
+                                            <img src="/img/Javascript-shield.svg"  alt="Skytsunami" />
+                                        </a>
+                                    </div>
                                 </div>
                             </v-col>
 
-                            <v-col class="skills-type" sm="12" md="4" xs="12">
-                                <h4>Professional</h4>
-                                <div class="text-left mt-10">
-                                    HTML
-                                    <v-progress-linear buffer-value="60" value="40"
-                                        stream color="white" class="mt-3" >
-                                    </v-progress-linear>
-                                    <br>
-                                    CSS
-                                    <v-progress-linear buffer-value="60" value="40"
-                                        stream color="white" class="mt-3" >
-                                    </v-progress-linear>
-                                    <br>
-                                    JavaScript
-                                    <v-progress-linear buffer-value="60" value="40"
-                                        stream color="white" class="mt-3" >
-                                    </v-progress-linear>
-                                    <br>
-                                    PHP
-                                    <v-progress-linear buffer-value="60" value="40"
-                                        stream color="white" class="mt-3" >
-                                    </v-progress-linear>
-                                    <br>
-                                    Node.js
-                                    <v-progress-linear buffer-value="60" value="40"
-                                        stream color="white" class="mt-3" >
-                                    </v-progress-linear>
+                            <v-col class="skills-type" sm="12" md="3" xs="12">
+                                <div class="container skills-container">
+                                    <div class="avatar">
+                                        <a href="#!">
+                                            <img src="/img/Javascript-shield.svg"  alt="Skytsunami" />
+                                        </a>
+                                    </div>
+                                </div>
+                            </v-col>
+
+                            <v-col class="skills-type" sm="12" md="3" xs="12">
+                                <div class="container skills-container">
+                                    <div class="avatar">
+                                        <a href="#!">
+                                            <img src="/img/Javascript-shield.svg"  alt="Skytsunami" />
+                                        </a>
+                                    </div>
                                 </div>
                             </v-col>
                         </v-row>
